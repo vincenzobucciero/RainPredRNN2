1a84b5c46ae0c2e8a4e6ee4b8cd4ce434e3adc9b
Francesco <74002478+r3aprz@users.noreply.github.com>
2025-03-27 09:42:06 +0100

    incremento dataset

--- run_conda.sh
+++ run_conda.sh
@@ -13,4 +13,9 @@ module load cuda/11.3
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate RainPredRNN
+
+# tensorboard --logdir=/home/f.demicco/RainPredRNN2/runs/RPRNN2 --host=0.0.0.0 &
+
 python3 -u source/app_transformer.py
+
+## ssh -L 6006:localhost:6006 f.demicco@193.205.230.3
