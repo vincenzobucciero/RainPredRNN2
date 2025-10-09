9109217ab98219e7ac03aaf95f10c2dba43f354b
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2025-10-09 10:02:48 +0200

    comment data path

--- source/app8.py
+++ source/app8.py
@@ -664,11 +664,8 @@ scaler = torch.cuda.amp.GradScaler('cuda', enabled=True)
 
 # === Main ===
 if __name__ == "__main__":
-<<<<<<< HEAD
-    DATA_PATH = os.path.abspath("/Users/vincenzobucciero/Desktop/RainPredRNN2/dataset_campania")    
-=======
+    # DATA_PATH = os.path.abspath("/Users/vincenzobucciero/Desktop/RainPredRNN2/dataset_campania")    
     DATA_PATH = os.path.abspath("/home/vbucciero/projects/RainPredRNN2/dataset_campania")    
->>>>>>> 34374ff (comm)
     CHECKPOINT_DIR = "checkpoints"
     os.makedirs(CHECKPOINT_DIR, exist_ok=True)
     
