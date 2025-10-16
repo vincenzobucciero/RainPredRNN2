b1381a87ead7e8c16a630604aef79c69a279bff0
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2025-10-16 17:07:11 +0200

    script to open tiff image

---
+++ open_image.py
@@ -0,0 +1,6 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+
+im = Image.open("/storage/external_01/hiwefi/data/rdr0_val_previews/epoch_000/predictions/frame_01_pred.tiff")
+plt.imshow(im, cmap='gray')
+plt.show()
