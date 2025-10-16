a06ab9dd8aac948df9ad67ba4f2183b481f23026
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2025-10-16 20:15:26 +0200

    open tiff script

--- open_image.py
+++ open_image.py
@@ -3,4 +3,5 @@ import matplotlib.pyplot as plt
 
 im = Image.open("/storage/external_01/hiwefi/data/rdr0_val_previews/epoch_000/predictions/frame_01_pred.tiff")
 plt.imshow(im, cmap='gray')
+
 plt.show()
